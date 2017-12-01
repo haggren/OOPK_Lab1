d394b96dacdee352db0505ad759197fb9b1c1b5f
wertysas <33478726+wertysas@users.noreply.github.com>
2017-12-01 10:06:19 +0100

    Add files via upload

---
+++ Lab4/src/lab4/Model.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4;
+
+/**
+ *
+ * @author joheric
+ */
+public class Model {
+    private static double length;
+    
+    public Model(int particleNum){
+        
+    }
+    
+    public void updateTotalPosition(){
+        
+    }
+    
+    public getTotalPosition(){
+        return
+    }
+    
+    private class Particle {
+    double xPos = 0;
+    double yPos = 0;
+    public Particle(double xPosin,double yPosin){
+        xPos = xPosin;
+        yPos = yPosin ;       
+    }
+    public Particle(){
+        this(Math.random(),Math.random());
+    }
+}
+
+    
+}
